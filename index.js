16035c7abbc184b93ae4162efa9d39a0a7763af6
Tanishka-bhatti <tanishkabhatti7@gmail.com>
2025-07-03 18:56:30 +0530

    middlewares

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
-const express = require('express');
-//used to add express or any other module to file 
+const express = require('express')
 const app = express()
-//including express module and initiaising an app
-//variable that stores oport number
-const port=3000;
+const port = 3000
+//loading middleware into the app
+///inbuilt middleware
+//app.use(express.json());
+//express.json()
+//middlewarre logging , authentication, validation
+//const loggingMiddleware = function(req,res,next){
+  //  console.log('login happening')
+    //next();
+//}
+//app.use(loggingMiddleware);
 
-app.get('/',(req,res)=>{
-    res.send('hello world!')
+//const authMiddleware = function (req,res,next){
+  //  console.log("authenticating")
+    //next();
+//}
+//app.use(authMiddleware);
+
+//const validateMiddleware = function(req,res,next){
+ //   console.log("validating the response ");
+   // res.send("chalo seedha ghar");
+    //next();
+    //we comment out next from here it wont go below
+//}
+//app.use(validateMiddleware);
+//order is important middlewares shud be written before the router
+
+
+
+const route=require("./routes/route")
+//mounting the routes to the app
+app.use('/api',route)
+
+
+
+
+
+app.get('/', (req, res) => {
+    console.log("i am a route handler");
+    console.log(req.body);
+  res.send('Hello World!')
 })
-//start your app
-app.listen(port,() => {
-    console.log("application start ho chuki hai");
-});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
